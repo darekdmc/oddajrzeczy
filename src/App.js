306788a79838a5bfb47c1c7fcf93dac87ee35ac0
Dariusz Zajac <darekdmc@gmail.com>
2020-04-29 23:49:43 +0200

    routing trial

--- src/App.js
+++ src/App.js
@@ -1,53 +1,41 @@
 import React from 'react';
 import {Route,HashRouter as Router,Switch,NavLink} from "react-router-dom"
 import NotFound from "./components/NotFound"
-/*
+
 const activeStyle = {
   fontWeight: "bold"
 };
 class Navigation extends React.Component {
   changeClick(e){
-    const menu = document.querySelector('.main-nav-list');
-    menu.classList.remove('show');
     window.scrollTo(0, 0)
   }
   render() {
     return (
         <>
-          <ul onClick={e => this.changeClick(e)} className="main-nav-list" id="menu">
-            <li><NavLink activeStyle={activeStyle} exact to="/">Strona główna</NavLink></li>
-            <li><NavLink activeStyle={activeStyle} to="/offer">Oferta</NavLink></li>
-            <li><NavLink activeStyle={activeStyle} to="/about">O nas</NavLink></li>
-            <li><NavLink activeStyle={activeStyle} to="/gallery">Galeria</NavLink></li>
-            <li><NavLink activeStyle={activeStyle} to="/contact">Kontakt</NavLink></li>
+          <ul onClick={e => this.changeClick(e)} className="top-nav-list" id="top-menu">
+            <li><NavLink activeStyle={activeStyle} to="/logowanie">Zaloguj</NavLink></li>
+            <li><NavLink activeStyle={activeStyle} to="/rejestracja">Załóż konto</NavLink></li>
           </ul>
         </>
     )
   }
 };
 
-class Button extends React.Component {
-
-  handleClick(e){
-    const menu = document.querySelector('.main-nav-list');
-    menu.classList.toggle('show');
-  }
-  render() {
+const Home = () => {
     return (
-        <button onClick={e => this.handleClick(e)} className="main-nav-toggle" aria-label="Pokaż menu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-    )
-  }
-}
+    <>
+        <HomeHeader/>
+        <HomeThreeColumns/>
+    </>
+)};
+
+
 const App = () => (
     <div>
       <Router>
         <header className="page-header">
           <h1 className="main-logo">
-            <a href="http://www.m-rdevelopment.pl">
+            <a href="/">
               <img src={logo} width="248" height="68" alt="Logo"/>
             </a>
           </h1>
@@ -58,10 +46,10 @@ const App = () => (
         </header>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route path="/offer" component={Offer} />
-          <Route path="/about" component={AboutUs} />
-          <Route path="/gallery" component={Gallery} />
-          <Route path="/contact" component={Contact} />
+          <Route path="/oddaj-rzeczy" component={Form} />
+          <Route path="/logowanie" component={Login} />
+          <Route path="/rejestracja" component={Register} />
+          <Route path="/wylogowano" component={Logout} />
           <Route component={NotFound} />
         </Switch>
         <footer>
@@ -71,8 +59,4 @@ const App = () => (
     </div>
 );
 
- */
-const App=()=>(
-    <h1>Sie ma</h1>
-)
 export default App;
