ecca60d6317d773afeae96732e15aec390c5d2ba
Dariusz Zajac <darekdmc@gmail.com>
2020-05-22 09:43:08 +0200

    login@register form

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React from "react";
 import LogNavigation from "./LogNavigation"
 
 import decoration from "../assets/Decoration.svg"
+import {HashLink as Link} from "react-router-hash-link";
 
 const Register = ()=>{
     return (
@@ -13,6 +14,28 @@ const Register = ()=>{
                 <h2>Załóż konto</h2>
                 <div className="page-header-main-decoration-wrapper">
                     <img src={decoration} width="260px" height="40px" alt="Decoration"/>
+                </div>
+                <form className="Regform-container">
+                    <div className="LogForm-wrapper">
+                        <div className="Log-email-wrapper">
+                            <label className="Log-labels">Email</label>
+                            <input className="Log-email"></input>
+                        </div>
+                        <div className="Log-email-wrapper">
+                            <label className="Log-labels">Hasło</label>
+                            <input className="Log-pass"></input>
+                        </div>
+                        <div className="Log-email-wrapper">
+                            <label className="Log-labels">Powtórz hasło</label>
+                            <input className="Log-pass"></input>
+                        </div>
+                    </div>
+                </form>
+                <div className="Log-button-wrapper">
+                    <Link to="/rejestracja">
+                        <button className="Reg-button">Zaloguj się</button>
+                    </Link>
+                    <button className="Reg-button1" >Załóż konto</button>
                 </div>
             </div>
         </>
