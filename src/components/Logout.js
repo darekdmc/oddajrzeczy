7d0b1b65dbe4794e92548c380f59b48136010bfb
Dariusz Zajac <darekdmc@gmail.com>
2020-05-22 09:53:53 +0200

    log-out

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,5 +1,25 @@
 import React from "react";
+import LogNavigation from "./LogNavigation";
+import decoration from "../assets/Decoration.svg";
+import {HashLink as Link} from "react-router-hash-link";
 const Logout = ()=>{
-    return <h1>Tutaj wylogowanie</h1>
+    return (
+        <>
+            <header className="LogHeader" id="LogHeader">
+                <LogNavigation/>
+            </header>
+            <div className="homeFourSteps-header-wrapper">
+                <h2>Wylogowanie nastąpiło<br/>pomyślnie!</h2>
+                <div className="page-header-main-decoration-wrapper">
+                    <img src={decoration} width="260px" height="40px" alt="Decoration"/>
+                </div>
+                <div className="Log-button-wrapper">
+                    <Link to="/">
+                        <button className="LogOut-button">Strona główna</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
 };
 export default Logout
